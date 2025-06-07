f8d4b7e44260f67e85720442e7bc98dd53e4e840
Mohamed Ramadan <mhmdrmdan652@gmail.com>
2025-06-07 17:01:55 +0300

    Initialize the project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',loadComponent:()=>import('./home/home.component').then(p=>p.HomeComponent)},
+    {path:'login',loadComponent:()=>import('./login/login.component').then(p=>p.LoginComponent)},
+];
